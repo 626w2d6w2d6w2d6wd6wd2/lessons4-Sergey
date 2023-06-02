7f8f22f312f9f78cd94ec010576f60c589965d40
sereg <seregakrivolap@gmail.com>
2023-06-02 20:46:33 +0300

    first koment 1234 new coment you

--- Src/lessons4.java
+++ Src/lessons4.java
@@ -1,7 +1,7 @@
 public class lessons4 {
     public static  void main(String[] args) {
 
-        System.out.println("Привет гит Я Серега" );
+        System.out.println("Привет гит Я Серега 1111" );
     }
 
 }
